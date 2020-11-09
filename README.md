a99b6ab617cdba8257364d7f08da337cc71414d8
myronalam <64386316+myronalam@users.noreply.github.com>
2020-11-08 23:38:39 -0800

    Updated csvmidi.py to accept multiple data series.  Added README file, sample data, and blank placeholder HTML file.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # csvmidi
-CSV to MIDI - CS50x Final Project
+## CSV to MIDI - CS50x Final Project Introduction
+
+The goal of this project is to allow a user to convert a CSV file to a MIDI file in order to hear a sound. This program is inspired by two episodes of the Marketplace radio program, when Kai Ryssdal spoke with the information science Ph.D student Jordan Wirfs-Brock to hear a "sonification" of the [rising unemployment](https://www.marketplace.org/2020/06/24/heres-what-the-crescendo-of-unemployment-sounds-like/) and [volatily of the stock market](https://www.marketplace.org/2020/03/31/the-sounds-of-a-volatile-stock-market/) during the COVID-19 pandemic.  One of my interests is in music, so I decided use this as a theme to explore in this project.
+
+## How to Use
+
+As of Nov 8, 2020, `csvmidi.py` is only functional via command line in a terminal. The way to run the program is:
+
+```
+python csvmidi.py data.csv outfile.mid
+```
+where:
+- `data.csv` is the input CSV file
+- `outfile.mid` is the output MIDI file
+
+Each data series is written as a separate MIDI track from left to right.
+
+CSV file assumptions:
+- The first column of data is a timestamp
+- Subsequent column(s) are data series
+- Each element in the data series is not null
+
+Python Package Requirements:
+- [MIDIUtil](https://midiutil.readthedocs.io/en/1.2.1/index.html)
+
+
+## TODO list
+These tasks will be refined as I encounter challenges beyond my current abilities.  After all, this project is expected to take the amount of effort as 2 problem sets in the course.  From my time spent on the previous assignments, I would imagine this project taking at least 24 hours of development time.
+
+I envision the following tasks will need to be completed to have a web app I can deploy:
+- [ ] Create python script to use MIDIUtil for generating MIDI files from CSV input
+    - [x] Basic command line operation takes CSV input
+    - [ ] Error checking of CSV file
+    - [ ] Add some ability to modify parameters (tempo, time)
+- [ ] Configure web server to run python with MIDIUtil installed and flask to serve webpage
+- [ ] Create front end webpage for User Interface
+- [ ] Figure out file uploads, error checking, size limits
+- [ ] Implement (static) data visualization using Plotly
+
+Nice to have:
+- [ ] Maybe allow user to pick a key
+- [ ] Consider 3rd party authentication for storing data
+- [ ] Tie project back to the Marketplace inspriation by connecting to the public data API provided by the Bureau of Labor Statistics
+
--- csvmidi.py
+++ csvmidi.py
@@ -6,7 +6,7 @@ from midiutil import MIDIFile
 def main():
     # Check usage, correct # of commmand line args
     if len(argv) != 3:
-        print("Usage: python csvmidi.py data.csv outfile")
+        print("Usage: python csvmidi.py data.csv outfile.mid")
         exit(1)
 
     # Get outfile name
@@ -19,9 +19,12 @@ def main():
         column_names = reader.fieldnames
     
     # (1a) Check # of series of data
-    if len(column_names) > 2:
-        print("Sorry, this program only handles 1 time series at this time.")
+    # Assumptions: Col1 = Timestamp, Col2, 3, etc are Data Series
+    """ Uncomment to impose restrictions on number of columns
+    if len(column_names) > 4:
+        print("Sorry, only 4 time series are supported at this time.")
         exit(2)
+    """
 
     # (2) Create ordered dictionary from CSV
     data = []
@@ -30,50 +33,56 @@ def main():
         for row in reader:
             data.append(row)
 
+    # Dictonary to store lists of scale degrees
+    score = {}
+    for track in range(len(column_names)-1):
+        
+        # TODO Need error handling to ensure non-numeric values filtered
+        values = [] # list of floats
+        for row in data:
+            values.append(float(row[column_names[track + 1]]))
+        
+        # Linear map from raw data to MIDI notes #36 (C2) to #96 (C7)
+        # y = m(x - x1) + y1
+        # Middle C is note #60
 
-    # TODO Need error handling to ensure non-numeric values filtered
-    values = []
-    for row in data:
-        values.append(float(row['Value']))
+        # Change these variable number based on desired range of notes
+        low_note = 36 # y1
+        high_note = 96 # y2
+        min_values = min(values) # x1
+        max_values = max(values) # x2
+        slope = (high_note - low_note) / (max_values - min_values)
+
+        degrees = [] # list of int (MIDI #)
+        for element in values:
+            degrees.append(int(round(slope * (element - min_values) + low_note)))
+                
+        score[track] = degrees
+
+    #print(score)
     
-    # Linear map from raw data to midi notes #36 (C2) to #96 (C7)
-    # y = m(x - x1) + y1
-    # Middle C is note #60
+    # Create number of tracks equal to num of CSV series inputed
+    MyMIDI = MIDIFile(len(column_names) - 1)
 
-    # Change these variable number based on desired range of notes
-    low_note = 36 # y1
-    high_note = 96 # y2
-    min_values = min(values) # x1
-    max_values = max(values) # x2
-    slope = (high_note - low_note) / (max_values - min_values)
-
-    degrees = [] # list of int (MIDI #)
-    for element in values:
-        degrees.append(int(round(slope * (element - min_values) + low_note)))
+    for track in score:
+        midi(MyMIDI, score[track], track)
     
-    print(degrees)
+    # Output MIDI file
+    with open(outfile_name, "wb") as output_file:
+        MyMIDI.writeFile(output_file)
+    
 
-    midi(degrees, outfile_name)
-
-
-def midi(degrees, outfile_name):
-    print("Midi function called")
-    print(outfile_name)
-    track    = 0
+def midi(MyMIDI, degrees, track):
+    print(f"Midi function called, writing track #{track}")
     channel  = 0
     time     = 0    # In beats
     duration = 1    # In beats
     tempo    = 90   # In BPM
-    volume   = 100  # 0-127, as per the MIDI standard
+    volume   = 110  # 0-127, as per the MIDI standard
 
-    MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
-                        # automatically)
     MyMIDI.addTempo(track, time, tempo)
 
     for i, pitch in enumerate(degrees):
         MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
-
-    with open(outfile_name, "wb") as output_file:
-        MyMIDI.writeFile(output_file)
     
 main()
